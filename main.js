00888a3c549f52f54972049ea1fb89bdd255787f
Lucas Eduardo Campolino <campolino.l@gmail.com>
2022-10-27 19:30:39 -0300

    add style and complete form

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ let myLibrary = [{ 'title': "Senhor da Chuva", 'author': "André Vianco", 'pages
 
 const page = document.querySelector('main');
 
-const add_button = document.querySelector('#add_book');
-add_button.addEventListener('click', () => {
-  page.appendChild(createForm());
+const toggleForm = document.querySelector('#add_book');
+toggleForm.addEventListener('click', () => {
+  let form = document.querySelector('form');
+  form.classList.toggle('visible');
+  toggleForm.textContent = 'Remove form'
 });
 
 function Book(title = "Blank Book", author = "Nobody", pages = 0, read = false ) {
@@ -48,40 +50,6 @@ function populateCard(book) {
   container.appendChild(pages);
   container.appendChild(read);
   return container;
-}
-
-function createForm() {
-  let form = document.createElement('form');
-  
-  let TL = document.createElement('input');
-  TL.setAttribute('type', 'text');
-  TL.setAttribute('name', 'Title');
-  TL.setAttribute('placeholder', 'Book title');
-  
-  let AU = document.createElement('input');
-  AU.setAttribute('type', 'text');
-  AU.setAttribute('name', 'Author');
-  AU.setAttribute('placeholder', 'Author name');
-  
-  let PG = document.createElement('input');
-  PG.setAttribute('type', 'number');
-  PG.setAttribute('name', 'Pages');
-  PG.setAttribute('placeholder', 'Number of pages');
-  
-  let RE = document.createElement('input');
-  RE.setAttribute('type', 'text');
-  RE.setAttribute('name', 'Read');
-  RE.setAttribute('placeholder', 'Already read?');
-
-  populateForm(form, TL, AU, PG, RE);
-  return form;
-}
-
-function populateForm(form, title, author, pages, read) {
-  form.appendChild(title);
-  form.appendChild(author);
-  form.appendChild(pages);
-  form.appendChild(read);
 }
 
 showLibrary();
